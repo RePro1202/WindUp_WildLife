db7e39d4eccdfdbe611e82a3e81aa1bb3f635465
jinha1544 <sonicx12369@gmail.com>
2025-04-20 01:42:21 +0900

    Update Character.cs

--- Assets/3 Scripsts/Character.cs
+++ Assets/3 Scripsts/Character.cs
@@ -1,13 +1,21 @@
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Experimental.Rendering;
 
 public class Character : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 3f;
+    private Rigidbody2D rb;
     private PlayerInput playerInput;
+    private MoveData presentMove;
+    private List<MoveData> moveQueue;
+    private int index = 0, indexcount;
+    private bool moveEnd = true;
+    public float descent = 0.4f;
 
     private void Awake()
     {
@@ -16,12 +24,41 @@ public class Character : MonoBehaviour
 
     public void MoveStart()
     {
-        List<MoveData> moveQueue = playerInput.GetMoveQueue();
+        moveQueue = playerInput.GetMoveQueue();
+        indexcount = moveQueue.Count;
+        presentMove = moveQueue[0];
+        moveEnd = false;
+        //StartCoroutine(ProcessMoveQueue(moveQueue));
 
-        StartCoroutine(ProcessMoveQueue(moveQueue));
     }
 
-    IEnumerator ProcessMoveQueue(List<MoveData> moveQueue)
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!moveEnd)
+        {
+            Vector2 moveDelta = presentMove.direction * moveSpeed * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + moveDelta);
+
+            presentMove.time -= descent * Time.fixedDeltaTime;
+            Debug.Log("½Ã°£: " + presentMove.time.ToString("F2"));
+
+            if (presentMove.time <= 0)
+            {
+                index += 1;
+                if (index < indexcount)
+                    presentMove = moveQueue[index];
+                else
+                    moveEnd = true;
+            }
+        }
+    }
+
+    /*IEnumerator ProcessMoveQueue(List<MoveData> moveQueue)
     {
         foreach (MoveData move in moveQueue)
         {
@@ -42,7 +79,7 @@ public class Character : MonoBehaviour
             timer += Time.deltaTime;
             yield return null;
         }
-    }
+    }*/
 
     public PlayerInput GetPlayerInput()
     {
