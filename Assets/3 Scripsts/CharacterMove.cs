b11795de2914108093311c35130fbd90887bb3ef
RePro1202 <astra852456@gmail.com>
2025-04-25 13:54:40 +0900

    충돌 처리 방식 변경.

--- Assets/3 Scripsts/CharacterMove.cs
+++ Assets/3 Scripsts/CharacterMove.cs
@@ -43,30 +43,71 @@ public class CharacterMove : MonoBehaviour
                 moveStartTime = Time.time;
             }
 
+            // 충돌하면 제자리에서 걷는 모션만 나오고 이동은 없음.
             if (!isBump)
             {
                 Vector3 moveDelta = curMoveData.direction * moveSpeed * Time.deltaTime;
                 transform.position = Vector3.MoveTowards(transform.position, transform.position + moveDelta, 0.1f);
+                BumpHandle(curMoveData.direction);
             }
-
-            BumpHandle();
         }
     }
 
-    private void BumpHandle()
-    {
-        List<Collider2D> coll = new List<Collider2D>();
-        boxCollider.Overlap(coll);
 
-        if (coll.Count > 0)
+    // 진행하던 방향에 따라 일정 값을 빼고 0.5단위로 반올림해서 위치 조정.
+    private void BumpHandle(Vector3 direction)
+    {
+        List<Collider2D> collider = new List<Collider2D>();
+        boxCollider.Overlap(collider);
+
+        if (collider.Count > 0)
         {
-            if (coll[0].tag == "Obstacle")
+            if (collider[0].tag == "Obstacle")
             {
                 isBump = true;
 
-                Debug.Log($"충돌 위치{transform.position}");
-                transform.position = RoundVector3ToStep(transform.position, 0.5f);
+                //Debug.Log($"충돌 위치:{transform.position}, 충돌 방향: {direction}");
+
+                Vector3 curPos = transform.position;
+                Vector3 target = new Vector3();
+
+                if (direction == Vector3.up)
+                {
+                    curPos.y -= 0.26f;
+
+                    target = RoundVector3ToStep(curPos, 0.5f);
+                    target.x = transform.position.x;
+                }
+                else if(direction == Vector3.down)
+                {
+                    curPos.y += 0.26f;
+
+                    target = RoundVector3ToStep(curPos, 0.5f);
+                    target.x = transform.position.x;
+                }
+                else if(direction == Vector3.left)
+                {
+                    curPos.x += 0.26f;
+
+                    target = RoundVector3ToStep(curPos, 0.5f);
+                    target.y = transform.position.y;
+                }
+                else if(direction == Vector3.right)
+                {
+                    curPos.x -= 0.26f;
+
+                    target = RoundVector3ToStep(curPos, 0.5f);
+                    target.y = transform.position.y;
+                }
+
+                transform.position = target;
+
+                //Debug.Log($"{collider.Count}");
             }
+        }
+        else
+        {
+            isBump = false;
         }
     }
 
@@ -94,6 +135,7 @@ public class CharacterMove : MonoBehaviour
 
     public void MoveEnd()
     {
+        BumpHandle(curMoveData.direction);
         isBump = false;
         queueIndex = 0;
         moveQueue.Clear();
